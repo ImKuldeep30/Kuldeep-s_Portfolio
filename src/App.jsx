d3f9886f5b04c387919d317485d2423fb80c4977
kuldeep <kuldpkohli2003@gmail.com>
2026-01-07 15:43:22 +0530

    custom cursor added

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useState, useRef, useEffect } from 'react'
 import Loader from './components/Loader'
 
 import Homepage from './pages/Homepage.jsx'
@@ -7,8 +7,25 @@ import Project from './pages/Project.jsx'
 import Certification from './pages/Certification.jsx'
 import Contactme from './pages/Contactme.jsx'
 
+import gsap from 'gsap'
+
 const App = () => {
   const [loading, setLoading] = useState(true)
+  const cursorRef = useRef(null)
+
+  useEffect(() => {
+    const moveCursor = (e) => {
+      gsap.to(cursorRef.current, {
+        x: e.clientX - 10,
+        y: e.clientY - 10,
+        duration: 0.15,
+        ease: "power1.out",
+      })
+    }
+
+    window.addEventListener("mousemove", moveCursor)
+    return () => window.removeEventListener("mousemove", moveCursor)
+  }, [])
 
   return (
     <>
@@ -16,6 +33,7 @@ const App = () => {
 
       {!loading && (
         <div>
+          <div ref={cursorRef} className="fixed top-0 left-0 h-8 w-8 bg-purple-300 rounded-full z-9999 pointer-events-none"></div>
           <Homepage />
           <Skills />
           <Project />
